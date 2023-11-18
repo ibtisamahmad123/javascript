4fbc719f10b8ddbce38e98b1e8d0999e923fbd4a
ibtisam <alexismozez123@gmail.com>
2023-11-18 12:12:34 +0500

    commited

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let name = nasir_hussain
+let place = karachi
+let age = 30
+let profession = software_engineer
+let job = AI_engineer
+let job_place = modulers
+let weight_kg = 70
+let height_ft = 5.8
+let income_usd = 1000
+
+let weight_pd = weight_kg*2.2
+
+console.log(weight_pd)
